2242b1540054e32dd2d48b918b305114f83f6052
Martina Masanova <martina.masanova@gmail.com>
2019-05-11 15:28:00 +0100

    style: follow pep8 for python files

--- database_setup.py
+++ database_setup.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
 
@@ -15,12 +16,13 @@ class User(Base):
 
     @property
     def serialize(self):
-      return {
-        'id': self.id,
-        'name': self.name,
-        'email': self.email,
-        'picture': self.picture
-      }
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'picture': self.picture
+        }
+
 
 class Category(Base):
     __tablename__ = 'category'
@@ -32,17 +34,18 @@ class Category(Base):
 
     @property
     def serialize(self):
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-           'user_id'      : self.user_id
-       }
+        return {
+            'name': self.name,
+            'id': self.id,
+            'user_id': self.user_id
+        }
+
 
 class Item(Base):
     __tablename__ = 'menu_item'
 
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     long_description = Column(String(1000))
     category_id = Column(Integer, ForeignKey('category.id'))
@@ -52,14 +55,14 @@ class Item(Base):
 
     @property
     def serialize(self):
-       return {
-           'id'                  : self.id,
-           'name'                : self.name,
-           'description'         : self.description,
-           'long_description'    : self.long_description,
-           'category_id'         : self.category_id,
-           'user_id'             : self.user_id
-       }
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'long_description': self.long_description,
+            'category_id': self.category_id,
+            'user_id': self.user_id
+        }
 
 engine = create_engine('sqlite:///item_catalog.db')
 
